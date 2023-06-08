9ae609e3a7d977b393440b7e31e33b812e9212fc
Tanmoym047 <tanmoym047@gmail.com>
2023-06-08 21:34:07 +0600

    Easy GUI Calculator

---
+++ android/app/src/main/kotlin/com/example/crap_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crap_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Calculator());
+}
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Crap Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SimpleCalculator(),
+    );
+  }
+}
+
+class SimpleCalculator extends StatefulWidget {
+  const SimpleCalculator({super.key});
+
+  @override
+  State<SimpleCalculator> createState() => _SimpleCalculatorState();
+}
+
+class _SimpleCalculatorState extends State<SimpleCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Crap Calculator"),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: Text(
+              "0",
+              style: TextStyle(fontSize: 38),
+            ),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+            child: Text(
+              "0",
+              style: TextStyle(fontSize: 48),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
